76c15a0bd71372e46dc1738e768b7b619b76d1ab
Ann S <404region@gmail.com>
2023-10-17 10:21:04 +0300

    Добавлен класс CarDao (но может он не нужен)

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,15 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Nissan", 999);
+      Car car2 = new Car("X", 777);
+      Car car3 = new Car("F", 888);
+      Car car4 = new Car("B", 777);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
